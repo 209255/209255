241c62d4ed2bf1389e16361f90e3e4d358a97a34
209255 <209255@student.pwr.edu.pl>
2015-05-27 15:22:59 +0200

    test

--- prj/src/Main.cpp
+++ prj/src/Main.cpp
@@ -17,7 +17,7 @@ int main()
   unsigned int Tablica_Rozmiarow[] = {50,100,500,1000,5000,10000,50000,100000,500000,1000000};
   Wyniki *W = new Wyniki(ILOSC_POW,ILOSC_PROB,Tablica_Rozmiarow);
   StrukturyBenchmark<int> *S = new StrukturyBenchmark<int>(ILOSC_PROB,ILOSC_POW,Tablica_Rozmiarow);
-  //S-> _Generator(10000000);
+  S-> _Generator(10000000);
   S -> _DodajObserwator(W);
   S -> _WykonajTest(E,"Dane.dat");
   W -> _ZapiszWyniki("Wyniki.dat");
